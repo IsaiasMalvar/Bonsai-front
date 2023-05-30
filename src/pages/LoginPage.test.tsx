5216b6937265f5bc46626008342cbf7725c737a6
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-05-30 18:11:47 +0200

    Add testing for LoginPage component

---
+++ src/pages/LoginPage.test.tsx
@@ -0,0 +1,16 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders, wrapWithRouter } from "../utils/testUtils";
+import LoginPage from "./LoginPage";
+
+describe("Given a LoginPage component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a heading with the text 'A blink, a story'", () => {
+      const expectedText = "A blink, a story";
+      renderWithProviders(wrapWithRouter(<LoginPage />));
+
+      const text = screen.getByRole("heading", { name: expectedText });
+
+      expect(text).toBeInTheDocument();
+    });
+  });
+});
