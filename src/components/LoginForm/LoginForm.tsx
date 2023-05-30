0d02aa5c6d34063a8d85129562f2881e16d75e6b
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-05-30 21:22:05 +0200

    Add own state to form controls

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
 import LoginFormStyled from "./LoginFormStyled";
 
 const LoginForm = (): React.ReactElement => {
+  const [loginData, setLoginData] = useState({ username: "", password: "" });
+  const onChangeUserData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginData({
+      ...loginData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
   return (
     <LoginFormStyled>
       <label htmlFor="username" className="form__label--username">
         Username:
       </label>
-      <input className="form__input--username" type="text" id="username" />
+      <input
+        className="form__input--username"
+        type="text"
+        id="username"
+        onChange={onChangeUserData}
+        value={loginData.username}
+      />
       <label htmlFor="password" className="form__label--password">
         Password:
       </label>
-      <input className="form__input--password" type="text" id="password" />
+      <input
+        className="form__input--password"
+        type="text"
+        id="password"
+        onChange={onChangeUserData}
+        value={loginData.username}
+      />
       <button type="submit" className="form__button--submit">
         Login
       </button>
