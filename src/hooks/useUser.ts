2533622b8968399f81fc3752083034c0237cc698
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-05-31 17:37:47 +0200

    Create useUser custom hook

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,25 @@
+import { useCallback } from "react";
+import axios from "axios";
+import { UserCredentials } from "../store/types";
+
+export const apiurl = import.meta.env.VITE_API_URL;
+
+const useUser = () => {
+  const getUserToken = useCallback(
+    async (userCrendentials: UserCredentials) => {
+      const {
+        data: { token },
+      } = await axios.post<{ token: string }>(
+        `${apiurl}/login`,
+        userCrendentials
+      );
+
+      return token;
+    },
+    []
+  );
+
+  return { getUserToken };
+};
+
+export default useUser;
