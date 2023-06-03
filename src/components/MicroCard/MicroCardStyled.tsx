f7225af6e705b77b1c6f1aa677b77767081483c7
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-06-03 19:58:38 +0200

    Add MicroCard styling

---
+++ src/components/MicroCard/MicroCardStyled.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+const MicroCardStyled = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  max-width: 500px;
+  min-height: 500px;
+  padding: 10px;
+  margin-top: 20px;
+  border-style: double;
+  background-color: black;
+  color: ${(prop) => prop.theme.colors.light};
+
+  .card-heading {
+    font-size: 30px;
+    margin-bottom: 10px;
+    margin-top: 20px;
+  }
+
+  .card-image {
+    margin-bottom: 10px;
+  }
+
+  .card-list {
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .card__list-item {
+    margin-bottom: 5px;
+    padding: 15px;
+    font-size: ${(prop) => prop.theme.fontSizes.regular};
+  }
+`;
+
+export default MicroCardStyled;
