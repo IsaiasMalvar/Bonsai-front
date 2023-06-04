8b523c44962b33f30e80a45c96dd719becfeca57
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-06-04 22:11:22 +0200

    Add showLoader and hideLoader in useMicro custom hook

--- src/hooks/useMicros/useMicros.ts
+++ src/hooks/useMicros/useMicros.ts
@@ -1,22 +1,38 @@
-import { useAppSelector } from "../../store";
+import { useAppDispatch, useAppSelector } from "../../store";
 import { useCallback } from "react";
 import { MicroStructure, MicrosApiResponse } from "../../store/types";
 import axios from "axios";
+import {
+  hideLoaderActionCreator,
+  showLoaderActionCreator,
+} from "../../store/ui/uiSlice";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const useMicros = () => {
+  const dispatch = useAppDispatch();
   const token = useAppSelector((state) => state.userStore.token);
 
-  const getMicros = useCallback(async (): Promise<MicroStructure[]> => {
-    const {
-      data: { microstories },
-    } = await axios.get<MicrosApiResponse>(`${apiUrl}/micros`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+  const getMicros = useCallback(async (): Promise<
+    MicroStructure[] | undefined
+  > => {
+    try {
+      dispatch(showLoaderActionCreator());
+      const {
+        data: { microstories },
+      } = await axios.get<MicrosApiResponse>(`${apiUrl}/micros`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
 
-    return microstories;
-  }, [token]);
+      dispatch(hideLoaderActionCreator());
+
+      return microstories;
+    } catch (error) {
+      dispatch(hideLoaderActionCreator());
+
+      throw new Error("Page could not load properly");
+    }
+  }, [dispatch, token]);
 
   return { getMicros };
 };
