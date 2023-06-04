46a71bf642d84ea2834345cddbd9c20e67f48ec3
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-06-04 22:53:48 +0200

    Add Loader component to MicrosPage

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,10 +4,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { userReducer } from "./user/userSlice";
 import { microsReducer } from "./micros/microsSlice";
+import { uiReducer } from "./ui/uiSlice";
 
 const rootReducer = combineReducers({
   userStore: userReducer,
   microsStore: microsReducer,
+  uiStore: uiReducer,
 });
 
 export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
