aea3914b214fb543d62966ffdf05e85fca1f91d0
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-06-06 13:57:27 +0200

    Add styling to NotFoundPage

---
+++ src/pages/NotFoundPage/NotFoundPageStyled.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const NotFoundPageStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+
+  .title {
+    font-size: ${(prop) => prop.theme.fontSizes.big};
+    margin-bottom: 50px;
+  }
+
+  .not-found__image {
+    border-radius: 10px;
+  }
+
+  .button__home {
+    position: sticky;
+    top: 100vh;
+    margin-top: 80px;
+    padding-bottom: 10px;
+    width: 100%;
+  }
+
+  .detail {
+    margin-top: 50px;
+    font-weight: bold;
+    font-size: ${(prop) => prop.theme.fontSizes.regular};
+  }
+`;
+
+export default NotFoundPageStyled;
