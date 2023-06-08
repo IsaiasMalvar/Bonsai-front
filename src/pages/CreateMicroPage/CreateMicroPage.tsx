37cbf753c954502189867d91f73e4a8f00f605ff
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-06-08 15:00:06 +0200

    Create CreateMicroPage component

---
+++ src/pages/CreateMicroPage/CreateMicroPage.tsx
@@ -0,0 +1,11 @@
+import CreatePageStyled from "./CreateMicroStyled";
+
+const CreateMicroPage = (): React.ReactElement => {
+  return (
+    <CreatePageStyled className="create-page">
+      <h1 className="create-page__title"> Add your micros</h1>
+    </CreatePageStyled>
+  );
+};
+
+export default CreateMicroPage;
