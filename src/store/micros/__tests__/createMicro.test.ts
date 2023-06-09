def233bc5e7ee8eacb64f37bfc8580b4de2f32da
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-06-09 22:19:43 +0200

    Add testing for createMicro reducer

---
+++ src/store/micros/__tests__/createMicro.test.ts
@@ -0,0 +1,24 @@
+import { microMock, microsMockList } from "../../../mocks/mocks";
+import { MicroStateStructure } from "../../types";
+import { createMicroActionCreator, microsReducer } from "../microsSlice";
+
+describe("Given a createMicro reducer", () => {
+  describe("When there is a list of micros in its current state and it receives one", () => {
+    test("Then it should add that micro to the list", () => {
+      const newMicro = microMock;
+
+      const currentMicrosState: MicroStateStructure = {
+        microstories: microsMockList,
+      };
+
+      const newMicroState = microsReducer(
+        currentMicrosState,
+        createMicroActionCreator(newMicro)
+      );
+
+      expect(newMicroState.microstories.length).toBe(
+        currentMicrosState.microstories.length + 1
+      );
+    });
+  });
+});
---
+++ src/store/micros/__tests__/deleteMicros.test.ts
@@ -0,0 +1,27 @@
+import { microsMockList } from "../../../mocks/mocks";
+import { MicroStateStructure } from "../../types";
+import { deleteMicroActionCreator, microsReducer } from "../microsSlice";
+
+describe("Given a deleteMicros reducer", () => {
+  describe("When it has a list of micros as a current state and it receives a valid micro id", () => {
+    test("Then it should return a new list without the micro linked to the id", () => {
+      const currentMicroState: MicroStateStructure = {
+        microstories: microsMockList,
+      };
+
+      microsMockList.shift();
+
+      const expectedNewMicroState = {
+        ...currentMicroState,
+        microstories: microsMockList,
+      };
+
+      const newMicroState = microsReducer(
+        currentMicroState,
+        deleteMicroActionCreator({ microId: "elfin" })
+      );
+
+      expect(newMicroState).toStrictEqual(expectedNewMicroState);
+    });
+  });
+});
