29e3ef65f17d2b04762a520823b3bebf34229465
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-06-11 20:11:01 +0200

    Add onSubmit function to MicroForm

--- src/pages/CreateMicroPage/CreateMicroPage.tsx
+++ src/pages/CreateMicroPage/CreateMicroPage.tsx
@@ -1,11 +1,26 @@
+import { useNavigate } from "react-router-dom";
 import MicroForm from "../../components/Form/MicroForm";
+import useMicros from "../../hooks/useMicros/useMicros";
+import { useAppDispatch } from "../../store";
 import CreateMicroPageStyled from "./CreateMicroPageStyled";
+import { MicroStructure } from "../../store/types";
+import { createMicroActionCreator } from "../../store/micros/microsSlice";
 
 const CreateMicroPage = (): React.ReactElement => {
+  const { createMicro } = useMicros();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const actionOnSubmit = async (newMicro: Partial<MicroStructure>) => {
+    const micro = await createMicro(newMicro);
+
+    dispatch(createMicroActionCreator(micro as MicroStructure));
+    navigate("/home");
+  };
   return (
     <CreateMicroPageStyled className="create-page">
       <h1 className="create-page__title"> Add your micro</h1>
-      <MicroForm />
+      <MicroForm actionOnSubmit={actionOnSubmit} />
     </CreateMicroPageStyled>
   );
 };
